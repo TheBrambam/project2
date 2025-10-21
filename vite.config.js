21c23f18323f80a83ca9745f11a8dbfdfd1cdb91
TheBrambam <anthony.brambilla@hotmail.com>
2025-10-21 16:07:49 -0400

    edited vite.config.js to auto update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/project2/', // GitHub pages
+  build: {
+    outDir: 'dist',    // default output folder for GitHub Pages
+  },
+  server: {
+    port: 5173,        // optional: local dev server port
+    open: true
+  }
 })
